9c7cd4915452fee5bcde7b8087f81810e929f445
Re-Note <dldydtn207@gmail.com>
2024-12-12 19:04:12 +0900

    feat : 프로젝트 폴더 초기 설정

---
+++ src/test/java/com/example/tricount_clone/TricountCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tricount_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TricountCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
